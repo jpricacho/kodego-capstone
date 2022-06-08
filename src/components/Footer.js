205186a3bc7429ad9e2d26b3670ed9a81efaf5be
jpricacho <jpricacho27@gmail.com>
2022-06-08 11:14:32 +0800

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { MDBFooter } from 'mdb-react-ui-kit';
 import BusinessSharpIcon from '@mui/icons-material/BusinessSharp';
 import EmailSharpIcon from '@mui/icons-material/EmailSharp';
 import CallSharpIcon from '@mui/icons-material/CallSharp';
 
 export default function App() {
   return (
-    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+    <div bgColor='light' className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
           {/* <span>Get connected with us on social networks:</span> */}
@@ -81,6 +80,6 @@ export default function App() {
           LuckyGTransport
         </a>
       </div>
-    </MDBFooter>
+    </div>
   );
 }
